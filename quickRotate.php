d28e721e7f225c2e9cb1b0131f970e4f8fb704c5
Calisto <35059233+Calisto1994@users.noreply.github.com>
2024-05-04 15:35:43 +0200

    Create quickRotate.php
    Initial commit

---
+++ quickRotate.php
@@ -0,0 +1,45 @@
+<?php
+    error_reporting(E_ALL);
+
+    $pattern = array("*.jpg"); // pattern (or multiple patterns) of the files you want to rotate
+    $degrees = 180; // how many degrees should the files become rotated?
+
+    echo("Rotating images...\n");
+
+    _readDir("./");
+
+    echo ("Done.\n");
+
+
+
+    function _readDir ($path) {
+        global $pattern;
+        echo("--- Directory ${path}\n");
+        
+        if (is_array($pattern)) { // multiple patterns?
+            foreach ($pattern as $glob)
+                rotateAllJPEG($path, $glob);    // rotate them all.
+        } else
+            rotateAllJPEG($path, "*.jpg");
+
+        foreach (glob("${path}*/") as $dirname) {
+            _readDir($dirname); // join into sub-directories and repeat what we're doing here;
+        }
+    }
+
+    function rotateAllJPEG ($path, $glob) {
+        global $degrees;
+
+        foreach (glob($path.$glob) as $filename) {
+            $image = imagecreatefromjpeg($filename); // open image file
+            $transparency = imagecolorallocatealpha($image, 0, 0, 0, 127);
+            // Transparency might be required if you're rotating not in 90-degrees steps but e.g. 45 degrees
+            $image = imagerotate($image, $degrees, $transparency); // rotate the image
+            echo("     ${filename} is being rotated...\n");
+
+            imagejpeg($image, $filename); // write image back to the file
+            unset($image); // free memory by deleting the variable
+        }
+    }
+
+?>
