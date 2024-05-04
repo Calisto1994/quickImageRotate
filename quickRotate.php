50fc1c166ffd90b3f97e4f9401e957dfe694bd36
Calisto <35059233+Calisto1994@users.noreply.github.com>
2024-05-04 18:05:57 +0200

    Update quickRotate.php
    Update: Changed quality level to 100 to avoid harsh compression of the image that results in ugly artifacts to appear in the finalized file

--- quickRotate.php
+++ quickRotate.php
@@ -2,11 +2,11 @@
 
     error_reporting(E_ALL);
 
-    $rotateAll = 0; // 0: Rotate all images; 1: Rotate only even images; 2: Rotate only odd images;
+    $rotateAll = 1; // 0: Rotate all images; 1: Rotate only even images; 2: Rotate only odd images;
     $pattern = array("*.[Jj][Pp][Gg]", "*.[Jj][Pp][Ee][Gg]"); // pattern (or multiple patterns) of the files you want to rotate
     // Instead of using an array, you may also just use this: $pattern = "*.jpg"; –– if you just need a single pattern.
     $degrees = 180; // how many degrees should the files become rotated?
-    $startPath = "./images/"; // default: the scripts very own directory ("./"), but can be set to any other.
+    $startPath = "./1927/"; // default: the scripts very own directory ("./"), but can be set to any other.
 
     echo("Rotating images...\n");
 
@@ -43,20 +43,20 @@
             if ($number % 2 == 0) { // Is it even?
                 if ($rotateAll == 0 || $rotateAll == 1) { // all, or only even images
                     $image = imagerotate($image, $degrees, $transparency); // rotate the image
-                    echo("     ${filename} is being rotated...\n");
+                    echo("         Rotating ${filename}\n");
                 } else {
-                    echo("     ${filename} is NOT being rotated...\n");
+                    echo("     Not Rotating ${filename}\n");
                 }
             } else { // or is it odd?
                 if ($rotateAll == 0 || $rotateAll == 2) { // all, or only odd images
                     $image = imagerotate($image, $degrees, $transparency); // rotate the image
-                    echo("     ${filename} is being rotated...\n");
+                    echo("         Rotating ${filename}\n");
                 } else {
-                    echo("     ${filename} is NOT being rotated...\n");
+                    echo("     Not Rotating ${filename}\n");
                 }
             }
 
-            imagejpeg($image, $filename); // write image back to the file
+            imagejpeg($image, $filename, 100); // write image back to the file
             unset($image); // free memory by deleting the variable
         }
     }
