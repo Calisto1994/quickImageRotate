3a62198120370070e1940c49cb6095a7fdf224f5
Calisto <35059233+Calisto1994@users.noreply.github.com>
2024-05-04 16:03:40 +0200

    Update quickRotate.php
    Allow for users to rotate only even images (1 not, 2, 3 not, 4, ...) on a per-directory basis

--- quickRotate.php
+++ quickRotate.php
@@ -1,10 +1,11 @@
 <?php
     error_reporting(E_ALL);
 
-    $pattern = array("*.jpg"); // pattern (or multiple patterns) of the files you want to rotate
+    $rotateAll = false; // if false, only even files are rotated (e.g. image 2, 4, 6, 8 and so on) per directory
+    $pattern = array("*.JPG"); // pattern (or multiple patterns) of the files you want to rotate
     // Instead of using an array, you may also just use this: $pattern = "*.jpg"; –– if you just need a single pattern.
     $degrees = 180; // how many degrees should the files become rotated?
-    $startPath = "./"; // default: the scripts very own directory ("./"), but can be set to any other.
+    $startPath = "./1927/"; // default: the scripts very own directory ("./"), but can be set to any other.
 
     echo("Rotating images...\n");
 
@@ -30,14 +31,24 @@
     }
 
     function rotateAllJPEG ($path, $glob) {
-        global $degrees;
+        global $degrees, $rotateAll;
+        $number = 0;
 
         foreach (glob($path.$glob) as $filename) {
             $image = imagecreatefromjpeg($filename); // open image file
             $transparency = imagecolorallocatealpha($image, 0, 0, 0, 127);
             // Transparency might be required if you're rotating not in 90-degrees steps but e.g. 45 degrees
-            $image = imagerotate($image, $degrees, $transparency); // rotate the image
-            echo("     ${filename} is being rotated...\n");
+            $number++;
+            if ($number % 2 == 0) {
+                if ($rotateAll == true) {
+                    $image = imagerotate($image, $degrees, $transparency); // rotate the image
+                    echo("     ${filename} is being rotated...\n");
+                } else 
+                    echo("     ${filename} is NOT being rotated...\n");
+            } else {
+                $image = imagerotate($image, $degrees, $transparency); // rotate the image
+                echo("     ${filename} is being rotated...\n");
+            }
 
             imagejpeg($image, $filename); // write image back to the file
             unset($image); // free memory by deleting the variable
